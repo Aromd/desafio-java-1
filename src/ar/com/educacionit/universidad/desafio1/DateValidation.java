68a6c6c45f19bdfc3e6eb0be446635ce2d4056aa
Javier Diaz <diazjavierdavid@hotmail.com>
2023-09-12 20:53:47 -0300

    finish exercise

--- src/ar/com/educacionit/universidad/desafio1/DateValidation.java
+++ src/ar/com/educacionit/universidad/desafio1/DateValidation.java
@@ -15,6 +15,25 @@ public class DateValidation {
         System.out.println("Ingrese el día en formato yyyy:");
         int year = teclado.nextInt();
 
+        teclado.close();
 
+        boolean isValidYear = year >= 1900 && year <= 2099;
+        boolean isValidMonth = month >= 1 && month <= 12;
+        boolean isValidDay = isValidDay(day, month, year);
+
+        String mensaje = isValidYear && isValidMonth && isValidDay ? "La fecha indicada es correcta" : "La fecha indicada es incorrecta";
+        System.out.println(mensaje);
+    }
+
+    public static boolean isValidDay(int day, int month, int year) {
+        boolean isEnteredYearLeap = ((year - 1904) % 4 == 0) && year != 1900;
+        System.out.println(isEnteredYearLeap);
+        boolean isMonthPair = month % 2 == 0;
+        boolean isFirstHalfOfTheYear = month <= 7;
+        boolean isLongMonth = (!isMonthPair && isFirstHalfOfTheYear) || (isMonthPair && !isFirstHalfOfTheYear);
+        int daysOfFebruary = isEnteredYearLeap ? 29 : 28;
+        int maxDaysAllowedInEnteredMonth = month == 2 ? daysOfFebruary : isLongMonth ? 31 : 30;
+
+        return day >= 1 && day <= maxDaysAllowedInEnteredMonth;
     }
 }
